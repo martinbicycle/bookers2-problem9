8969b7949dddb33fd6475daaa4b45c0776028b6a
martinbicycle <martinbicycle7700@gmail.com>
2021-01-03 09:20:09 +0000

    [Add]コードチェック

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -7,19 +7,18 @@ class BookCommentsController < ApplicationController
     @book_comment.book_id = @book.id
     @book_comment.user_id = current_user.id
     unless @book_comment.save
-      render 'error'  # app/views/book_comments/error.js.erbを参照する ※要件外
+      render 'error'
     end
-    # app/views/book_comments/create.js.erbを参照する
   end
 
   def destroy
     @book = Book.find(params[:book_id])
     book_comment = @book.book_comments.find(params[:id])
     book_comment.destroy
-    # app/views/book_comments/destroy.js.erbを参照する
   end
 
   private
+
   def book_comment_params
     params.require(:book_comment).permit(:comment)
   end
